d112b678f93c5a67680e412aa8caec6e9a7408ae
Taylor Massey <tamassey92@gmail.com>
2023-01-04 20:53:31 -0700

    commit tree 84e868403d59c5aba7c5bfab9b65ed742a8f5aa6

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(name, email){
+fetch("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: name,
+        email: email,
+    })
+  })
+.then((res) => res.json())
+.then((data) => data.id.append())
+}
+
+
+  
+
+
